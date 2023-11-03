d3aab5e2145f377d78da6dee9c94fc1aaab7fabb
elliotbradly <elliotbradly@gmail.com>
2023-11-02 20:53:34 -0400

    write sprite working

--- src/composables/screens.ts
+++ src/composables/screens.ts
@@ -4,7 +4,7 @@ import * as ActVsg from '../110.shade/01.visage.unit/visage.action'
 import * as ActCan from '../110.shade/03.container.unit/container.action'
 import * as ActTxt from '../110.shade/05.text.unit/text.action'
 import * as ActGph from '../110.shade/04.graphic.unit/graphic.action'
-
+import * as ActSpr from '../110.shade/06.sprite.unit/sprite.action'
 
 
 export type HelloWorld = string | number
@@ -51,6 +51,9 @@ export const update = async (value: HelloWorld) => {
 
   bit = await SHADE['hunt']( ActTxt.WRITE_TEXT, { idx:'txt03', dat: {  txt: "text 03", y:45 }  })
   bit = await SHADE['hunt']( ActCan.ADD_CONTAINER, { idx: "can00",  dat:{bit:bit.txtBit.dat.bit }})
+
+  bit = await SHADE['hunt']( ActSpr.WRITE_SPRITE, { idx:'spr00', dat: { src:'./img/000.png',  x:40, y:80 }  })
+  bit = await SHADE['hunt']( ActCan.ADD_CONTAINER, { idx: "can00",  dat:{bit:bit.sprBit.dat.bit }})
 
   bit = await SHADE['hunt']( ActGph.WRITE_GRAPHIC, { idx:'gph00', dat: { h:100, w:40, x:40, y:40 }  })
   bit = await SHADE['hunt']( ActCan.ADD_CONTAINER, { idx: "can00",  dat:{bit:bit.gphBit.dat.bit }})
